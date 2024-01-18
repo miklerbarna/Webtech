56e6c7408dd8837fbd6ad302b94141fb8c28c2b4
leventezhu <30834450+leventezhu@users.noreply.github.com>
2024-01-18 14:05:18 +0100

    Added map for Customer Rental

--- bike-rental-frontend/src/app/customer-rental-view/customer-rental-view.component.ts
+++ bike-rental-frontend/src/app/customer-rental-view/customer-rental-view.component.ts
@@ -1,48 +1,45 @@
-import { Component, OnInit  } from '@angular/core';
-import { LeafletModule } from '@asymmetrik/ngx-leaflet';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 import * as L from 'leaflet';
 
 @Component({
   selector: 'app-customer-rental-view',
   standalone: true,
-  imports: [LeafletModule],
+  imports: [],
   templateUrl: './customer-rental-view.component.html',
   styleUrl: './customer-rental-view.component.css'
 })
-export class CustomerRentalViewComponent implements OnInit {
-  map: L.Map;
+export class CustomerRentalViewComponent implements AfterViewInit {
+  private map?: L.Map; 
+  private stations = [
+    { lat: 46.62472, lon: 14.30528, name: 'Central Station', desc: "#4285" },
+    { lat: 46.66472, lon: 14.30228, name: 'Viktring Station', desc: "#4285"},
+  ];
   mapOptions: L.MapOptions = {
     zoom: 12,
     center: new L.LatLng(46.62472, 14.30528)
   };
 
   constructor() {
-    this.map = new L.Map('map', this.mapOptions);
+    
+  }
+  ngAfterViewInit(): void {
+    this.initMap();
   }
 
-  ngOnInit(): void {
-    // Define your map options and initialize the map here
-    if (this.map){
-      const stations = [
-        { lat: 46.62472, lon: 14.30528, name: 'Central Station' },
-        // ... other stations ...
-      ];
+  private initMap(): void {
+    this.map = new L.Map('map').setView([46.62472, 14.30528], 13);
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 18
+    }).addTo(this.map);
 
-      this.map = new L.Map('map', {
-        layers: [
-          L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            maxZoom: 18
-          })
-        ],
-        zoom: 13,
-        center: L.latLng(46.62472, 14.30528) // Centered at Klagenfurt
-      });
-
-      stations.forEach(station => {
-        L.marker([station.lat, station.lon]).addTo(this.map)
-          .bindPopup(station.name);
-      });
+    this.addStationsToMap();
   }
+
+  private addStationsToMap(): void {
+    this.stations.forEach(station => {
+      L.marker([station.lat, station.lon]).addTo(this.map!)
+        .bindPopup(station.name);
+    });
   }
 }
 
